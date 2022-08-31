719d1a9759db3b39b825c41ac8fb2d418491091a
Shaheen Khan <fshaheenkhan@gmail.com>
2022-08-31 09:46:32 -0400

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './Card.css';
+
+const Card = ({ name, image }) => {
+  const [{ angle, xPos, yPos }] = useState({
+    angle: Math.random() * 90 - 45,
+    xPos: Math.random() * 40 - 20,
+    yPos: Math.random() * 40 - 20
+  });
+  const transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+  return <img className="Card" 
+              src={image} 
+              alt={name} 
+              style={{ transform }} />;
+};
+
+export default Card;
